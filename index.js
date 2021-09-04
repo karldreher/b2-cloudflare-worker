ce4573ab0c2e2793e4dc351dddcea2fd8c3e2376
Karl Dreher <karldreher@outlook.com>
2021-09-04 15:22:22 -0700

    Linting

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event));
+});
 
 async function getValueFromKV(key) {
   let value = await kv_namespace.get(key);
   return value;
 }
 
-async function authorizeAccount(){
-  authValue = await(getValueFromKV('AUTH_HEADER'))
+async function authorizeAccount() {
+  authValue = await getValueFromKV("AUTH_HEADER");
   authHeader = {
-    'Authorization': authValue
-  }
-  const account = await fetch('https://api.backblazeb2.com/b2api/v2/b2_authorize_account', {
-    headers: authHeader
-  })
-  .then( res => res.json() )
-  return account
+    Authorization: authValue,
+  };
+  const account = await fetch(
+    "https://api.backblazeb2.com/b2api/v2/b2_authorize_account",
+    {
+      headers: authHeader,
+    }
+  ).then((res) => res.json());
+  return account;
 }
 
-
 async function serveAsset(event) {
-  const url = new URL(event.request.url)
+  const url = new URL(event.request.url);
 
   //First determine if the cache has the object already
-  const cache = caches.default
-  let response = await cache.match(event.request)
+  const cache = caches.default;
+  let response = await cache.match(event.request);
 
   if (!response) {
     //get api url and auth from b2
-    auth = await authorizeAccount()
-    
+    auth = await authorizeAccount();
+
     //Set target URL
     const url = new URL(event.request.url);
     //Bucket name comes from KV
-    bucketName = await(getValueFromKV('BUCKET_NAME'))
+    bucketName = await getValueFromKV("BUCKET_NAME");
     //Request URL to B2 must contain the download URL returned from authorize account, then the path /file/{bucketname}/{pathname}
-    requestURL = new URL(auth.downloadUrl + '/file/' + bucketName +  url.pathname)
-    params = { "b2CacheControl": 'public,max-age=86400'}
-    requestURL.search = new URLSearchParams(params).toString()
+    requestURL = new URL(
+      auth.downloadUrl + "/file/" + bucketName + url.pathname
+    );
+    params = { b2CacheControl: "public,max-age=86400" };
+    requestURL.search = new URLSearchParams(params).toString();
     requestHeaders = {
-      "Authorization" : auth.authorizationToken,
-    }
+      Authorization: auth.authorizationToken,
+    };
     response = await fetch(requestURL, {
-      headers: requestHeaders
-    })
+      headers: requestHeaders,
+    });
 
-    response = new Response(response.body, response)
-    response.headers.set("Cache-Control", "public,max-age=86400")
+    response = new Response(response.body, response);
+    response.headers.set("Cache-Control", "public,max-age=86400");
 
-    event.waitUntil(cache.put(event.request, response.clone()))
+    event.waitUntil(cache.put(event.request, response.clone()));
   }
-  return response
+  return response;
 }
 
-
 async function handleRequest(event) {
-  let response = await serveAsset(event)
+  let response = await serveAsset(event);
   if (response.status > 399) {
-    response = new Response(response.statusText, { status: response.status })
+    response = new Response(response.statusText, { status: response.status });
   }
-  return response
+  return response;
 }
